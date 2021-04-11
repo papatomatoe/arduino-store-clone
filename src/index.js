cc3e60b1b6aa21d184a8a97e1adf946c8518c36a
papatomatoe <hole_in_the_wall@list.ru>
2021-04-11 14:15:36 +0300

    add: redux store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import App from './components/App';
+import store from "./store";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./components/App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
