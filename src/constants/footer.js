360b0d1a7086edff6bbdb4408780343659ce145d
papatomatoe <hole_in_the_wall@list.ru>
2021-04-18 14:20:07 +0300

    add:  constants

---
+++ src/constants/footer.js
@@ -0,0 +1,14 @@
+export const INFO_LINKS = [
+  { title: "Shipping Policy", link: "/shipping-policy" },
+  { title: "Payment Methods", link: "/payment-methods" },
+  { title: "Transparency", link: "/transparency" },
+];
+
+export const SOCIAL_LINKS = [
+  { key: "fb", title: "facebook", link: "/" },
+  { key: "inst", title: "instagram", link: "/" },
+  { key: "tw", title: "twitter", link: "/" },
+  { key: "gh", title: "github", link: "/" },
+  { key: "in", title: "linked in", link: "/" },
+  { key: "yt", title: "youtube", link: "/" },
+];
---
+++ src/constants/header.js
@@ -0,0 +1,10 @@
+export const MAIN_LINKS = [
+  { title: "boards", link: "/boards" },
+  { title: "shields", link: "/shields" },
+  { title: "kits", link: "/kits" },
+  { title: "accessories", link: "/accessories" },
+];
+export const USER_BTNS = [
+  { title: "Cart", link: "/cart" },
+  { title: "Favorites", link: "/favorites" },
+];
