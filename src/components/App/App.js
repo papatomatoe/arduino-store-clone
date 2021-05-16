31c233c6154118b24b42673197c1cc963e755b3d
papatomatoe <hole_in_the_wall@list.ru>
2021-05-16 14:12:42 +0300

    add: Boards route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import MainPage from "../pages/Main";
 import LoginPage from "../pages/Login";
+import ProductGroup from "../pages/ProductGroup";
 import Error404 from "../pages/Error404";
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Switch>
         <Route path="/" exact component={MainPage} />
         <Route path="/login" component={LoginPage} />
+        <Route path="/boards" exact component={ProductGroup} />
         <Route component={Error404} />
       </Switch>
     </BrowserRouter>
